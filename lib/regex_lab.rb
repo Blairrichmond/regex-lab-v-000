9608df35de5b2191a610ed84a343178532f89656
Brent  Ransom <sadumedusa@gmail.com>
2018-01-24 23:35:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
   "afoot Excellent incredible Absolute unreal Inconceivable octopus".match(/^[aeiou][AEIOU]/)
+  return true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
