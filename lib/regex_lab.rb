95aa71d1a12d92521cf232a89359b22bddb52285
Brent  Ransom <sadumedusa@gmail.com>
2018-01-24 23:57:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,5 +16,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.grip(/[\D+]/, "").length == 10 ? true : false
+  phone.grep(/[\D+]/, "").length == 10 ? true : false
 end
